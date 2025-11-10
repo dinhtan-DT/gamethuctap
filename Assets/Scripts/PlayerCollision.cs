bcef29dedc23d64f244077ae88ba886b4f18a9d6
Nguyễn Đình Tấn <dinhtanck58@gmail.com>
2025-11-11 05:57:15 +0700

    thêm map mới

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -9,5 +9,9 @@ public class PlayerCollision : MonoBehaviour
         {
             SceneManager.LoadScene("Question");
         }
+        else if (collision.CompareTag("Answer"))
+        {
+            SceneManager.LoadScene("GameOver"); // ← đổi tên cho trùng với tên Scene bạn đã tạo
+        }
     }
 }
